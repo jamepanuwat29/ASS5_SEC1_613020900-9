16f8b97daa6795dd1d149a8378b0e95f197b94de
jamepanuwat29 <70094893+jamepanuwat29@users.noreply.github.com>
2020-09-06 18:07:36 +0700

    Eiei

---
+++ app/src/main/java/com/example/lab5intent/Student.kt
@@ -0,0 +1,7 @@
+package com.example.lab5intent
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Employee( val Name: String, val gender: String,val Email: String,val salary: Int) : Parcelable {
+}
